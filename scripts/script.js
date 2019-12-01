ceda7ffef2955dfa65c420589a2e0d859fd12af4
Victor <severin.victor@yandex.ru>
2019-12-01 19:53:42 +0300

    Task 3_functions commit

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@
 let money,
     time;
 
+// Функция для получения данных о бюджете на месяц
 function start() {
     money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
@@ -14,6 +15,7 @@ function start() {
 
 start();
 
+// Объект с данными
 let appData = {
         budget: money,
         expenses: {},
@@ -23,6 +25,7 @@ let appData = {
         savings: true
     };
 
+// Функция для получени данных о статьях расходов
 function chooseExpences() {
     for (let i = 0; i < 2; i++){
         let a = prompt("Введите обязательную статью расходов в этом месяце"),
@@ -86,39 +89,48 @@ let i = 0;
 // } while (i < 2);
 */
 
+// Функция для расчета дневного бюджета
 function detectDayBudget() {
     appData.moneyPerDay = (appData.budget / 30).toFixed();
-    alert("Ежедневный бюджет: " + appData.moneyPerDay);
+    alert("Ежедневный бюджет: " + appData.moneyPerDay + "руб.");
 }
 
+// Функция для расчета уровня достатка
 function detectLevel() {
     if (appData.moneyPerDay < 100) {
-        console.log("Минимальный уровень достатка");
+        console.log("Это минимальный уровень достатка");
     } else if ( (appData.moneyPerDay > 100) && (appData.moneyPerDay < 2000) ) {
-        console.log("Средний уровень достатка");
+        console.log("Это средний уровень достатка");
     } else if (appData.moneyPerDay > 2000) {
-        console.log("Высокий уровень достатка");
+        console.log("Это высокий уровень достатка");
     } else {
         console.log("Произошла ошибка");
     }
 }
 
+detectLevel();
+
+// Функция для расчета дохода от накоплений в зависимости от процента
 function checkSavings() {
     if (appData.savings == true) {
         let save = +prompt("Какова сумма накопления?"),
             percent = +prompt("Под какой процент?");
         
         appData.monthIncome = save / 100 / 12 * percent;
+        alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
     }
 }
 
 checkSavings();
 
+// Функция для определения необязательных расходов
 function chooseOptExpenses() {
     for (let i = 1; i < 4; i++) {
         let answer = prompt("Статья необязательных расходов?");
+        
         appData.optionalExpenses[i] = answer;
+        console.log(appData.optionalExpenses);
     }  
 }
 
-// chooseOptExpenses();
+chooseOptExpenses();
